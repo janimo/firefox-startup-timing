402960740319c5c321c2e29158b20fa219c842fa
Jani Monoses <jani@ubuntu.com>
2011-05-20 16:08:38 +0300

    Initial automated test script for firefox startup timing, using the about:startup add-on

---
+++ firefox_startup_timing.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Firefox extensions directory
+EXTDIR=~/.mozilla/extensions/\{ec8030f7-c20a-464f-9b0e-13a3a9e97384\}/
+
+#"About:startup" extension location
+XPIDIR=aboutstartup@glandium.org.xpi
+
+#Default profile directory
+PROFILEDIR=/home/jani/.mozilla/firefox/*.default/
+
+#Logfile created by the extension
+LOGFILE=$PROFILEDIR/startup.log
+
+#Save original preferences
+cp $PROFILEDIR/prefs.js .
+
+rm -f $LOGFILE
+
+#Set autoQuit option
+cp user.js $PROFILEDIR
+
+#Install extension
+mv $XPIDIR $EXTDIR
+
+#Quietly flush pagechace, dentries and inodes, to simulate a cold start
+sudo sync && sudo sysctl -q -w vm.drop_caches=3
+
+#Cold start
+firefox 2>/dev/null
+#Warm start 1
+firefox 2>/dev/null
+#Warm start 2
+firefox 2>/dev/null
+
+#Remove extension
+mv $EXTDIR/$XPIDIR .
+
+#Output the timing measurements
+cat $LOGFILE
+
+#Cleanup
+rm $LOGFILE
+rm $PROFILEDIR/user.js
+mv prefs.js $PROFILEDIR/
+
---
+++ user.js
@@ -0,0 +1,2 @@
+user_pref("extensions.aboutstartup.autoQuit", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
